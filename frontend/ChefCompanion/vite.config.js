7796cc6632fc5e8c5376af45481b140c531b61b3
VNA818-RPI <Arcerv@rpi.edu>
2023-10-08 22:24:49 -0400

    inital frontend setup

---
+++ frontend/ChefCompanion/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      clientPort: 3000
+    },
+    port: 3000,
+    watch: {
+      usePolling: true
+    }
+  }
+})
