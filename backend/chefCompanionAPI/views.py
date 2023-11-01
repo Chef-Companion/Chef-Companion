8301073069ae6af68b75653c19ecba3f0002e9a3
Viano <arcerv@rpi.edu>
2023-10-31 21:04:43 -0400

    endpoint to return all recipes

--- backend/chefCompanionAPI/views.py
+++ backend/chefCompanionAPI/views.py
@@ -22,41 +22,47 @@ def unique_ingredients(request):
     return JsonResponse({'unique_ingredients': unique_ingredients})
 
 # Return recipes
-@api_view(["POST"])
+@api_view(["POST", "GET"])
 def recipes(request):
-    body = json.loads(request.body)
-    """
-    Example POST:
-    "Content-Type": "application/json"
-    {
-        ingredients: ['flour', 'soda', 'salt', 'sugar', 'egg', 'margarine', 'buttermilk'],
-        ingredient_mode: 'exact', # Match each ingredient exactly, if not 'exact' will assume contains
-        ingredients_mode: 'exact' # Only return recipes with the ingredients you have and no extra ingredients, if not 'exact' will assume contains
-    }
-    """
-    # if json post does not include ingredients or ingredients is not an array
-    if 'ingredients' not in body or type(body['ingredients']) != list:
-        # Return status 400 "Bad Request"
-        return HttpResponse("Missing ingredients array", status=400)
-    ingredients = body['ingredients']
-    ingredients_mode = body.get('ingredients_mode', '')
-    ingredient_mode = body.get('ingredient_mode', '')
-    query = Q() # new Query
-    for ingredient in ingredients:
-        if ingredient_mode == 'exact':
-            # NER in the database looks like this: '["broccoli", "bacon", "green onions", "raisins", "mayonnaise", "vinegar", "sugar"]'
-            # We can search for an exact ingredient by searching for '"ingredient"'
-            query.add(Q(ner__icontains=f'\"{ingredient}\"'), Q.AND)
-        else:
-            # Search for containing ingredient for example a recipie with "green onions" would be returned when searching for onions
-            query.add(Q(ner__icontains=ingredient), Q.AND)
-    # Query the database
-    objects = Recipes.objects.filter(query).all()
-    if ingredients_mode == 'exact':
-        # Exact length meaning the only recipes with the ingredients inputted and no extra ingredients
-        objects = list(filter(lambda x: x.ingredientsLength() == len(ingredients), objects))
-    # Build json response
-    return JsonResponse({ "result": [object.to_dict() for object in objects] })
+    if request.method == "GET":
+        # Get all recipes
+        objects = Recipes.objects.all()
+        return JsonResponse({ "result": [object.to_dict() for object in objects] })
+    if request.method == "POST":
+        # Query for recipes
+        body = json.loads(request.body)
+        """
+        Example POST:
+        "Content-Type": "application/json"
+        {
+            ingredients: ['flour', 'soda', 'salt', 'sugar', 'egg', 'margarine', 'buttermilk'],
+            ingredient_mode: 'exact', # Match each ingredient exactly, if not 'exact' will assume contains
+            ingredients_mode: 'exact' # Only return recipes with the ingredients you have and no extra ingredients, if not 'exact' will assume contains
+        }
+        """
+        # if json post does not include ingredients or ingredients is not an array
+        if 'ingredients' not in body or type(body['ingredients']) != list:
+            # Return status 400 "Bad Request"
+            return HttpResponse("Missing ingredients array", status=400)
+        ingredients = body['ingredients']
+        ingredients_mode = body.get('ingredients_mode', '')
+        ingredient_mode = body.get('ingredient_mode', '')
+        query = Q() # new Query
+        for ingredient in ingredients:
+            if ingredient_mode == 'exact':
+                # NER in the database looks like this: '["broccoli", "bacon", "green onions", "raisins", "mayonnaise", "vinegar", "sugar"]'
+                # We can search for an exact ingredient by searching for '"ingredient"'
+                query.add(Q(ner__icontains=f'\"{ingredient}\"'), Q.AND)
+            else:
+                # Search for containing ingredient for example a recipie with "green onions" would be returned when searching for onions
+                query.add(Q(ner__icontains=ingredient), Q.AND)
+        # Query the database
+        objects = Recipes.objects.filter(query).all()
+        if ingredients_mode == 'exact':
+            # Exact length meaning the only recipes with the ingredients inputted and no extra ingredients
+            objects = list(filter(lambda x: x.ingredientsLength() == len(ingredients), objects))
+        # Build json response
+        return JsonResponse({ "result": [object.to_dict() for object in objects] })
 
 class ingredientRequest(generics.ListCreateAPIView):
     queryset = IngredientList.objects.all()
