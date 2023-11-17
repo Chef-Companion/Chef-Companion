556eef5447023c89d229ef29dcda18e66d804c1b
Stanagley <stanleyguo333@gmail.com>
2023-11-17 13:42:02 -0500

    added more instructions to the popout window

--- frontend/ChefCompanion/src/views/GettingStarted.jsx
+++ frontend/ChefCompanion/src/views/GettingStarted.jsx
@@ -5,7 +5,7 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 function GettingStartedModal({ isOpen, onRequestClose }) {
-    // Define custom styles for the modal
+    // Define custom styles
     const modalStyles = {
         content: {
             maxWidth: '400px', 
@@ -16,17 +16,22 @@ function GettingStartedModal({ isOpen, onRequestClose }) {
     };
 
     return (
-        // Create the modal component
+        // Create the popout window
         <Modal
             isOpen={isOpen}                 
             onRequestClose={onRequestClose} 
             contentLabel="Getting Started Modal"
             style={modalStyles}              
         >
-            <h2>Getting Started</h2>  
-            Test
+            <h3>Welcome!</h3>  
             <p>
-                Welcome to Chef Companion! Generate recipes based on what you have from your pantry and kitchen.
+                Welcome to Chef Companion! Generate recipes based on what you have from your pantry and kitchen. 
+            </p>
+            <h3>Generating Recipes</h3>
+            <p>
+                To use this application, please exit this window and click on the Main Page button.
+                Use the search bar to find ingredients and add them to the list. Select desired ingredient to 
+                generate recipes with those ingredients.
             </p>
             <button onClick={onRequestClose}>Close</button> 
         </Modal>
